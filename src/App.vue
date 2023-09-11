9ba90129ee685ca9f84f3b46824754c80da01348
Gino804 <giacomo.p03@gmail.com>
2023-09-11 16:05:17 +0200

    Added api call

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup>
+<script>
+import axios from 'axios';
+const endpoint = 'http://127.0.0.1:8000/api/projects';
 
+export default {
+  data: () => ({ projects: [] }),
+
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => { this.projects = res.data })
+    }
+  },
+
+  created() {
+    this.fetchProjects();
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <ul>
+    <li v-for="project in projects">{{ project.title }}</li>
+  </ul>
+</template>
 
 <style></style>
