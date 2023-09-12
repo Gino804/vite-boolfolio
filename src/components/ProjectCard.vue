90bada785cea0bc77802278cb0e12b6cd57b91b5
Gino804 <giacomo.p03@gmail.com>
2023-09-12 17:02:36 +0200

    Added project detail page

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,14 +2,18 @@
 export default {
     name: 'ProjectCard',
 
-    props: { project: Object },
+    props: { project: Object, isDetail: Boolean },
 }
 </script>
 
 <template>
     <div class="card my-4">
-        <div class="card-header">
-            Project {{ project.id }}
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <span>Project {{ project.id }}</span>
+            <RouterLink v-if="!isDetail" class="btn btn-success"
+                :to="{ name: 'project-detail', params: { id: project.id } }">See
+            </RouterLink>
+            <RouterLink v-else class="btn btn-secondary" to="/">Back to list</RouterLink>
         </div>
         <div class="card-body">
             <h5 class="card-title">{{ project.title }}</h5>
--- src/pages/ProjectDetailPage.vue
+++ src/pages/ProjectDetailPage.vue
@@ -1,5 +1,29 @@
-<script></script>
+<script>
+import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
+const baseUri = 'http://127.0.0.1:8000/api/projects/';
+export default {
+    name: 'ProjectDetail',
+
+    components: { ProjectCard },
+
+    data: () => ({
+        project: null
+    }),
+
+    methods: {
+        getProject() {
+            const endpoint = baseUri + this.$route.params.id;
+            axios.get(endpoint).then(res => { this.project = res.data });
+        }
+    },
+
+    created() {
+        this.getProject();
+    }
+}
+</script>
 
 <template>
-    <h1>Dettaglio</h1>
+    <ProjectCard :project="project" :isDetail="true" />
 </template>
