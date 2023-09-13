24b72bc362f99aef7f888cb65edf0a7c2f5fc06d
Gino804 <giacomo.p03@gmail.com>
2023-09-13 15:43:59 +0200

    Added mail sending

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,5 +1,40 @@
-<script></script>
+<script>
+import axios from 'axios';
+const endpoint = 'http://127.0.0.1:8000/api/contact-message';
+const emptyForm = {
+    email: '',
+    subject: '',
+    message: ''
+}
+
+export default {
+    name: 'ContactPage',
+    data: () => ({
+        form: emptyForm
+    }),
+    methods: {
+        sendForm() {
+            axios.post(endpoint, this.form).then(() => { this.form = emptyForm })
+        }
+    }
+}
+</script>
 
 <template>
-    Contact
+    <h1>Contact</h1>
+    <form @submit.prevent="sendForm">
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="email" v-model.trim="form.email">
+        </div>
+        <div class="mb-3">
+            <label for="subject" class="form-label">Subject</label>
+            <input type="text" class="form-control" id="subject" v-model.trim="form.subject">
+        </div>
+        <div class="mb-3">
+            <label for="message" class="form-label">Message</label>
+            <textarea class="form-control" id="message" rows="5" v-model.trim="form.message"></textarea>
+        </div>
+        <button class="btn btn-primary">Submit</button>
+    </form>
 </template>
